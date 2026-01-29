522628b69aae6ceb2330e7c5ec102bf1d16893c8
xutongle <xutongle@msn.com>
2026-01-29 19:01:43 +0800

    feat: 添加中英文系统语言文件

---
+++ lang/en/system.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * This is NOT a freeware, use is subject to license terms.
+ */
+
+declare(strict_types=1);
+
+return [
+    'successful_operation' => 'Operation successful',
+];
---
+++ lang/zh_CN/system.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms.
+ */
+
+declare(strict_types=1);
+
+return [
+    'successful_operation' => '操作成功',
+];
