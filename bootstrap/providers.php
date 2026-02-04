aa3d08f5dd1ae2c1b66d130221262aea2daca5c3
xutongle <xutongle@msn.com>
2026-02-04 19:05:01 +0800

    feat: 添加Filament后台管理功能及相关资源
    refactor: 调整服务提供者注册顺序
    style: 格式化composer.json文件
    chore: 添加Filament依赖及构建脚本

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,8 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\DynamicConfigServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FileServiceProvider::class,
-    App\Providers\SmsServiceProvider::class,
-    //取消自动注册TelescopeServiceProvider，因为在本地环境下，需要手动注册
-    //App\Providers\TelescopeServiceProvider::class,
     App\Providers\OpenAIProvider::class,
+    App\Providers\SmsServiceProvider::class,
 ];
